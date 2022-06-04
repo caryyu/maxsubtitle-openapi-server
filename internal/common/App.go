c00a3b9c23eb7b1eb0e64e62e6e87039370af666
caryyu <caryyu.tg@gmail.com>
2022-06-04 16:40:29 +0800

    Add cache mechanism

--- internal/common/App.go
+++ internal/common/App.go
@@ -3,9 +3,11 @@ package common
 import (
 	"github.com/caryyu/subtitle-open-server/internal/resource"
 	"github.com/gorilla/mux"
+	cache "github.com/victorspringer/http-cache"
 )
 
 type App struct {
-	Router  *mux.Router
-	Fetcher *resource.A4kDotNet
+	Router      *mux.Router
+	Fetcher     *resource.A4kDotNet
+	CacheClient *cache.Client
 }
